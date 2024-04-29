7ea862fa5e6920734953eb6f20e944b8804595c7
Daniel C <daniel@clayson.io>
2024-04-29 14:06:17 -0400

    Move speaker data into separate file

--- netlify/functions/2024_H1.mts
+++ netlify/functions/2024_H1.mts
@@ -3,137 +3,7 @@ import {Feed} from "@numbered/feed";
 import dayjs from "dayjs";
 import {parseCronExpression} from "cron-schedule";
 import Mixpanel from 'mixpanel';
-
-const posts: {url: string, speaker: string}[] = [
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1030-jeffrey-r-holland-32k-eng.mp3?download=true",
-        "speaker": "jeffrey-r-holland"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1040-j-anette-dennis-32k-eng.mp3?download=true",
-        "speaker": "j-anette-dennis"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1050-alexander-dushku-32k-eng.mp3?download=true",
-        "speaker": "alexander-dushku"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1060-ulisses-soares-32k-eng.mp3?download=true",
-        "speaker": "ulisses-soares"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1070-jack-n-gerard-32k-eng.mp3?download=true",
-        "speaker": "jack-n-gerard"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1080-henry-b-eyring-32k-eng.mp3?download=true",
-        "speaker": "henry-b-eyring"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2010-david-a-bednar-32k-eng.mp3?download=true",
-        "speaker": "david-a-bednar"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2020-massimo-de-feo-32k-eng.mp3?download=true",
-        "speaker": "massimo-de-feo"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2030-brent-h-nielson-32k-eng.mp3?download=true",
-        "speaker": "brent-h-nielson"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2040-jose-l-alonso-32k-eng.mp3?download=true",
-        "speaker": "jose-l-alonso"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2050-gerrit-w-gong-32k-eng.mp3?download=true",
-        "speaker": "gerrit-w-gong"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2060-michael-t-nelson-32k-eng.mp3?download=true",
-        "speaker": "michael-t-nelson"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2070-quentin-l-cook-32k-eng.mp3?download=true",
-        "speaker": "quentin-l-cook"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3010-shayne-m-bowen-32k-eng.mp3?download=true",
-        "speaker": "shayne-m-bowen"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3020-steven-r-bangerter-32k-eng.mp3?download=true",
-        "speaker": "steven-r-bangerter"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3030-andrea-munoz-spannaus-32k-eng.mp3?download=true",
-        "speaker": "andrea-munoz-spannaus"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3040-matthew-l-carpenter-32k-eng.mp3?download=true",
-        "speaker": "matthew-l-carpenter"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3050-dieter-f-uchtdorf-32k-eng.mp3?download=true",
-        "speaker": "dieter-f-uchtdorf"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4010-ronald-a-rasband-32k-eng.mp3?download=true",
-        "speaker": "ronald-a-rasband"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4020-susan-h-porter-32k-eng.mp3?download=true",
-        "speaker": "susan-h-porter"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4030-dale-g-renlund-32k-eng.mp3?download=true",
-        "speaker": "dale-g-renlund"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4040-paul-b-pieper-32k-eng.mp3?download=true",
-        "speaker": "paul-b-pieper"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4050-patrick-kearon-32k-eng.mp3?download=true",
-        "speaker": "patrick-kearon"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4060-brian-k-taylor-32k-eng.mp3?download=true",
-        "speaker": "brian-k-taylor"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4070-dallin-h-oaks-32k-eng.mp3?download=true",
-        "speaker": "dallin-h-oaks"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5010-d-todd-christofferson-32k-eng.mp3?download=true",
-        "speaker": "d-todd-christofferson"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5020-taylor-g-godoy-32k-eng.mp3?download=true",
-        "speaker": "taylor-g-godoy"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5030-gary-e-stevenson-32k-eng.mp3?download=true",
-        "speaker": "gary-e-stevenson"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5040-mathias-held-32k-eng.mp3?download=true",
-        "speaker": "mathias-held"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5050-neil-l-andersen-32k-eng.mp3?download=true",
-        "speaker": "neil-l-andersen"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5060-mark-l-pace-32k-eng.mp3?download=true",
-        "speaker": "mark-l-pace"
-    },
-    {
-        "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5070-russell-m-nelson-32k-eng.mp3?download=true",
-        "speaker": "russell-m-nelson"
-    }
-];
+import {Posts_2024_H1} from './data/2024_H1';
 
 export default async (req: Request, context: Context) => {
 
@@ -148,7 +18,7 @@ export default async (req: Request, context: Context) => {
         var mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN);
 
 // Note: you must supply the user_id who performed the event in the `distinct_id` field
-        mixpanel.track("Signed Up", {
+        mixpanel.track("Accessed RSS Feed", {
             distinct_id: stamp || 'unknown',
             type: 'RSS Feed',
             cron,
@@ -189,16 +59,19 @@ export default async (req: Request, context: Context) => {
     }
 
     while(true){
-        const post = posts[iterator];
+        const post = Posts_2024_H1[iterator];
         const date = cronGenerator.next().value;
 
-        if(!date || !isValidDate(date)) break;
+        if(!post || !date || !isValidDate(date)) break;
         feed.addItem({
-            title: post.speaker,
+            title: post.title,
             id: post.url,
             link: post.url,
             description: post.speaker,
-            author: [],
+            author: [{
+                name: post.speaker,
+                email: ''
+            }],
             date: dayjs(date).subtract(1, 'day').startOf('day').toDate(),
             image: post.image,
             enclosure: {
@@ -210,7 +83,7 @@ export default async (req: Request, context: Context) => {
 
 
         iterator++;
-        if(iterator >= posts.length) break;
+        if(iterator >= Posts_2024_H1.length) break;
     }
 
 // Output: RSS 2.0
--- netlify/functions/data/2024_H1.ts
+++ netlify/functions/data/2024_H1.ts
@@ -1,130 +1,162 @@
-[
+export const Posts_2024_H1: {url: string, speaker: string, title: string}[] = [
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1030-jeffrey-r-holland-32k-eng.mp3?download=true",
-    "speaker": "jeffrey-r-holland"
+    "speaker": "President Jeffrey R. Holland",
+    "title": "Motions of a Hidden Fire"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1040-j-anette-dennis-32k-eng.mp3?download=true",
-    "speaker": "j-anette-dennis"
+    "speaker": "Sister J. Anette Dennis",
+    "title":  "Put Ye On the Lord Jesus Christ"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1050-alexander-dushku-32k-eng.mp3?download=true",
-    "speaker": "alexander-dushku"
+    "speaker": "Elder Alexander Dushku",
+    "title":  "Pillars and Rays"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1060-ulisses-soares-32k-eng.mp3?download=true",
-    "speaker": "ulisses-soares"
+    "speaker": "Elder Ulisses Soares",
+    "title":  "Covenant Confidence through Jesus Christ"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1070-jack-n-gerard-32k-eng.mp3?download=true",
-    "speaker": "jack-n-gerard"
+    "speaker": "Elder Jack N. Gerard: ‘Integrity",
+    "title":  " Christlike Attribute"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-1080-henry-b-eyring-32k-eng.mp3?download=true",
-    "speaker": "henry-b-eyring"
+    "speaker": "President Henry B. Eyring",
+    "title":  "All Will Be Well Because of Temple Covenants"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2010-david-a-bednar-32k-eng.mp3?download=true",
-    "speaker": "david-a-bednar"
+    "speaker": "Elder David A. Bednar",
+    "title":  "Be Still, and Know That I Am God"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2020-massimo-de-feo-32k-eng.mp3?download=true",
-    "speaker": "massimo-de-feo"
+    "speaker": "Elder Massimo De Feo",
+    "title":  "Rise! He Calleth Thee"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2030-brent-h-nielson-32k-eng.mp3?download=true",
-    "speaker": "brent-h-nielson"
+    "speaker": "Elder Brent H. Nielson",
+    "title":  "A Record of What I Have Both Seen and Heard"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2040-jose-l-alonso-32k-eng.mp3?download=true",
-    "speaker": "jose-l-alonso"
+    "speaker": "Elder Jose L. Alonso",
+    "title":  "Jesus Christ at the Center of Our Lives"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2050-gerrit-w-gong-32k-eng.mp3?download=true",
-    "speaker": "gerrit-w-gong"
+    "speaker": "Elder Gerrit W. Gong",
+    "title":  "All Things for Your Good"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2060-michael-t-nelson-32k-eng.mp3?download=true",
-    "speaker": "michael-t-nelson"
+    "speaker": "Brother Michael T. Nelson",
+    "title":  "Relationships Influence Choices"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-2070-quentin-l-cook-32k-eng.mp3?download=true",
-    "speaker": "quentin-l-cook"
+    "speaker": "Elder Quentin L. Cook",
+    "title":  "Be One With Christ"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3010-shayne-m-bowen-32k-eng.mp3?download=true",
-    "speaker": "shayne-m-bowen"
+    "speaker": "Elder Shayne M. Bowen",
+    "title":  "Miracles, Angels and Priesthood Power"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3020-steven-r-bangerter-32k-eng.mp3?download=true",
-    "speaker": "steven-r-bangerter"
+    "speaker": "Elder Steven R. Bangerter",
+    "title":  "Foreordained To Serve"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3030-andrea-munoz-spannaus-32k-eng.mp3?download=true",
-    "speaker": "andrea-munoz-spannaus"
+    "speaker": "Sister Andrea Muñoz Spannaus",
+    "title":  "Faithful to the End"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3040-matthew-l-carpenter-32k-eng.mp3?download=true",
-    "speaker": "matthew-l-carpenter"
+    "speaker": "Elder Matthew L. Carpenter",
+    "title":  "Fruit That Remains"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-3050-dieter-f-uchtdorf-32k-eng.mp3?download=true",
-    "speaker": "dieter-f-uchtdorf"
+    "speaker": "Elder Dieter F. Uchtdorf",
+    "title":  "A Higher Joy"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4010-ronald-a-rasband-32k-eng.mp3?download=true",
-    "speaker": "ronald-a-rasband"
+    "speaker": "Elder Ronald A. Rasband",
+    "title":  "Words Matter"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4020-susan-h-porter-32k-eng.mp3?download=true",
-    "speaker": "susan-h-porter"
+    "speaker": "President Susan H. Porter",
+    "title":  "Pray, He Is There"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4030-dale-g-renlund-32k-eng.mp3?download=true",
-    "speaker": "dale-g-renlund"
+    "speaker": "Elder Dale G. Renlund",
+    "title":  "The Powerful, Virtuous Cycle of the Doctrine of Christ"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4040-paul-b-pieper-32k-eng.mp3?download=true",
-    "speaker": "paul-b-pieper"
+    "speaker": "Elder Paul B. Pieper",
+    "title":  "Trust in the Lord"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4050-patrick-kearon-32k-eng.mp3?download=true",
-    "speaker": "patrick-kearon"
+    "speaker": "Elder Patrick Kearon",
+    "title":  "God’s Intent is to Bring You Home"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4060-brian-k-taylor-32k-eng.mp3?download=true",
-    "speaker": "brian-k-taylor"
+    "speaker": "Elder Brian K. Taylor",
+    "title":  "Swallowed Up in the Joy of Christ"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-4070-dallin-h-oaks-32k-eng.mp3?download=true",
-    "speaker": "dallin-h-oaks"
+    "speaker": "President Dallin H. Oaks",
+    "title":  "Covenants and Responsibilities"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5010-d-todd-christofferson-32k-eng.mp3?download=true",
-    "speaker": "d-todd-christofferson"
+    "speaker": "Elder D. Todd Christofferson",
+    "title":  "The Testimony of Jesus"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5020-taylor-g-godoy-32k-eng.mp3?download=true",
-    "speaker": "taylor-g-godoy"
+    "speaker": "Elder Taylor G. Godoy",
+    "title":  "Call, Don’t Fall"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5030-gary-e-stevenson-32k-eng.mp3?download=true",
-    "speaker": "gary-e-stevenson"
+    "speaker": "Elder Gary E. Stevenson",
+    "title":  "Bridging the Two Great Commandments"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5040-mathias-held-32k-eng.mp3?download=true",
-    "speaker": "mathias-held"
+    "speaker": "Elder Mathias Held",
+    "title":  "Opposition in All Things"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5050-neil-l-andersen-32k-eng.mp3?download=true",
-    "speaker": "neil-l-andersen"
+    "speaker": "Elder Neil L. Andersen",
+    "title":  "Temples, Houses of the Lord Dotting the Earth"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5060-mark-l-pace-32k-eng.mp3?download=true",
-    "speaker": "mark-l-pace"
+    "speaker": "President Mark L. Pace",
+    "title":  "It Is Wisdom in the Lord That We Should Have the Book of Mormon"
   },
   {
     "url": "https://media2.ldscdn.org/assets/general-conference/april-2024-general-conference/2024-04-5070-russell-m-nelson-32k-eng.mp3?download=true",
-    "speaker": "russell-m-nelson"
+    "speaker": "President Russell M. Nelson",
+    "title":  "Rejoice in the Gift of Priesthood Keys"
   }
 ]
