e13a6d655ff03077216b2ebcd77f035bfc0ee1a1
Daniel Clayson <daniel@dabblefox.com>
2025-04-11 15:51:23 -0400

    feat: Add 2025_04 and an action

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # RSS Builder
 
-This is a simple RSS feed builder for conference talks of [The Church of Jesus Christ of Latter Day Saints](https://www.churchofjesuschrist.org/). It's a simple tool to create a custom RSS feed based on a date and schedule.
+This is a simple RSS feed builder for conference talks of [The Church of Jesus Christ of Latter Day Saints](https://www.churchofjesuschrist.org/). 
+It's a simple tool to create a custom RSS feed based on a date and schedule.
+
+After each conference, someone needs to go in and run `npm run addLatestSession` to add the latest session to the RSS feed.
+I've tried to set this to auto-run using github actions, but it might not work. Please log an issue if you need me to run it.
---
+++ netlify/functions/data/2025_04.ts
@@ -0,0 +1,195 @@
+const Posts_2025_04 = [
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/13holland?lang=eng",
+    "title": "As a Little Child",
+    "speaker": "Jeffrey R. Holland",
+    "audioUrl": "https://assets.churchofjesuschrist.org/n80cwfgri9360z4n0mckkekxkfa5v530a8v4nyj0-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/14johnson?lang=eng",
+    "title": "Spiritually Whole in Him",
+    "speaker": "Camille N. Johnson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/owl56cnh58bk1ui0gma6f3zghd6eaapl1e3ez84v-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/15rasband?lang=eng",
+    "title": "Right Before Our Eyes",
+    "speaker": "Ronald A. Rasband",
+    "audioUrl": "https://assets.churchofjesuschrist.org/uuuij8v7wzdv2ri8na8y7pbh6gbnvuemv58r22uz-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/16cook?lang=eng",
+    "title": "The Atonement of Jesus Christ Provides the Ultimate Rescue",
+    "speaker": "Quentin L. Cook",
+    "audioUrl": "https://assets.churchofjesuschrist.org/nf96ca666opotxw763q936t4o6bcnni92vaywur8-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/17gimenez?lang=eng",
+    "title": "My Love for the Savior Is My  Why ",
+    "speaker": "Ricardo P. Gim nez",
+    "audioUrl": "https://assets.churchofjesuschrist.org/7f6xhpd8lraheb37nx0k0s2im1eozdojrfixte14-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/18eyring?lang=eng",
+    "title": " Draw Near unto Me ",
+    "speaker": "Henry B. Eyring",
+    "audioUrl": "https://assets.churchofjesuschrist.org/m5sbush15vn9q9vwnbxbmpvtlmri3t9z7wt6ogn5-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/21andersen?lang=eng",
+    "title": "Cherishing Life",
+    "speaker": "Neil L. Andersen",
+    "audioUrl": "https://assets.churchofjesuschrist.org/yr4k91z673pennwvpfqeswcyls52unx8d8mugdk6-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/22lund?lang=eng",
+    "title": "Divine Authority, Sublime Young Men",
+    "speaker": "Steven J. Lund",
+    "audioUrl": "https://assets.churchofjesuschrist.org/vnfm7z4wgxghen03fbq6vdn1hyv4zqw4inum40e7-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/23palmer?lang=eng",
+    "title": " Return unto Me   That I May Heal You ",
+    "speaker": "S. Mark Palmer",
+    "audioUrl": "https://assets.churchofjesuschrist.org/oaapkp8b4h7ly94tiwmnxabr5hgfcby4z4ofu6z4-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/24roman?lang=eng",
+    "title": "Faith: A Bond of Trust and Loyalty",
+    "speaker": "Sandino Roman",
+    "audioUrl": "https://assets.churchofjesuschrist.org/3w4fxovugalbameagzjp5iikogksd8010wokghkt-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/25renlund?lang=eng",
+    "title": "Personal Preparation to Meet the Savior",
+    "speaker": "Dale G. Renlund",
+    "audioUrl": "https://assets.churchofjesuschrist.org/ybh3tc2okfae515nxw9wetm8f97jmkgv2egtcs8y-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/26boom?lang=eng",
+    "title": "True to the Faith That Our Parents Have Cherished",
+    "speaker": "Hans T. Boom",
+    "audioUrl": "https://assets.churchofjesuschrist.org/lo6o01bbgk7wzx82whp0hmabxrvbyj9mna9jzmgv-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/27uchtdorf?lang=eng",
+    "title": " By This All Will Know That You Are My Disciples ",
+    "speaker": "Dieter F. Uchtdorf",
+    "audioUrl": "https://assets.churchofjesuschrist.org/ehdsilc6f0wnrar5g667l6pduisutxog8ryoo5b8-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/31stevenson?lang=eng",
+    "title": "And We Talk of Christ",
+    "speaker": "Gary E. Stevenson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/92ca8u414zt6qc1owqynhtaf5zr9el9qc3vt2013-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/32wright?lang=eng",
+    "title": "Thou Art the Christ",
+    "speaker": "Amy A. Wright",
+    "audioUrl": "https://assets.churchofjesuschrist.org/q2a45wncbuijio5ku8qkebt9at6qq7w5tblj0v14-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/33rasband?lang=eng",
+    "title": "The Plan of Mercy",
+    "speaker": "James R. Rasband",
+    "audioUrl": "https://assets.churchofjesuschrist.org/4m372a6gr9mot39viz4fkny0vy3ghlubv43187zl-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/34vargas?lang=eng",
+    "title": "Our Heavenly Guidance System",
+    "speaker": "Sergio R. Vargas",
+    "audioUrl": "https://assets.churchofjesuschrist.org/pex4ab0te4d2imwqt1bcd9wctk1ca219wzkfka2j-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/35christofferson?lang=eng",
+    "title": "Worship",
+    "speaker": "D. Todd Christofferson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/5ifwhkl9k5nbyixo9xzi0eyn0jt40pyuo73w7lxw-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/41bednar?lang=eng",
+    "title": "The Times of Restitution of All Things",
+    "speaker": "David A. Bednar",
+    "audioUrl": "https://assets.churchofjesuschrist.org/r46og7k28aswbixsngenforwafuqhehjhlzob8oe-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/42shumway?lang=eng",
+    "title": "Participate to Prepare for Christ s Return",
+    "speaker": "Steven D. Shumway",
+    "audioUrl": "https://assets.churchofjesuschrist.org/41b4wyoo23xv2wt1mriszd7snn8h1j2mcotjgwnv-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/43runia?lang=eng",
+    "title": "Your Repentance Doesn t Burden Jesus Christ; It Brightens His Joy",
+    "speaker": "Tamara W. Runia",
+    "audioUrl": "https://assets.churchofjesuschrist.org/heqnsn5duxkuz0cymm4yvtso3jpghwpgc62bp2zz-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/44causse?lang=eng",
+    "title": "Compensating Blessings",
+    "speaker": "G rald Causs ",
+    "audioUrl": "https://assets.churchofjesuschrist.org/x2ts0yilnyx2weepm108uq397xohu05wtc2j9gig-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/45gong?lang=eng",
+    "title": "Eternity s Great Gifts: Jesus Christ s Atonement, Resurrection, Restoration",
+    "speaker": "Gerrit W. Gong",
+    "audioUrl": "https://assets.churchofjesuschrist.org/px1yfsy99q4pgeykiu48hvuj8pcfyvnmk9f9waj7-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/46mccune?lang=eng",
+    "title": "Joy Through Covenant Discipleship",
+    "speaker": "John A. McCune",
+    "audioUrl": "https://assets.churchofjesuschrist.org/wtsjajrd7159d8xscoalqdqkwa30l6l3eo205bp2-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/47oaks?lang=eng",
+    "title": "Divine Helps for Mortality",
+    "speaker": "Dallin H. Oaks",
+    "audioUrl": "https://assets.churchofjesuschrist.org/bejbw1zvw9ps834nn2ck5ehis1grnmmrbng8j0d5-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/51soares?lang=eng",
+    "title": "Reverence for Sacred Things",
+    "speaker": "Ulisses Soares",
+    "audioUrl": "https://assets.churchofjesuschrist.org/ubkg5a8qc97oqsw2whdim5ohgf02cqzqb1z15zft-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/52strong?lang=eng",
+    "title": "Charity a Sign of True Discipleship",
+    "speaker": "Michael B. Strong",
+    "audioUrl": "https://assets.churchofjesuschrist.org/6muv1mmga6hswngllzfcy5dj3qylcadoknw8ru4y-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/53whiting?lang=eng",
+    "title": "Beware the Second Temptation",
+    "speaker": "Scott D. Whiting",
+    "audioUrl": "https://assets.churchofjesuschrist.org/6vf2jk7zqurj68q5yza9koya7ckr0vtpyfgk3bkc-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/54kim?lang=eng",
+    "title": "Harden Not Your Heart",
+    "speaker": "Christopher H. Kim",
+    "audioUrl": "https://assets.churchofjesuschrist.org/lit6n8suaj0jd81a1nm0h01dmzrhaa0exfajytv1-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/55kearon?lang=eng",
+    "title": "Receive His Gift",
+    "speaker": "Patrick Kearon",
+    "audioUrl": "https://assets.churchofjesuschrist.org/0jurznp3gze5uof77govqq2gqpgrdgagpaooctsi-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/56tai?lang=eng",
+    "title": "The Love of God",
+    "speaker": "Benjamin M. Z. Tai",
+    "audioUrl": "https://assets.churchofjesuschrist.org/nhugqslb27a4afqvdfcz66f5t5m9g6fr8tlja3ab-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/04/57nelson?lang=eng",
+    "title": "Confidence in the Presence of God",
+    "speaker": "Russell M. Nelson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/v4s5aqokcv8ap0q8cva0qdtvtpdiz5awrtlo7kiq-128k-en.mp3?download=true"
+  }
+] 
+ export default Posts_2025_04
