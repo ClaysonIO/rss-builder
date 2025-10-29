555692bfcab9005b50c84ce4e0951142b9d28e3d
Daniel Clayson <daniel@dabblefox.com>
2025-10-28 22:28:54 -0400

    feat: Add October 2025

---
+++ netlify/functions/data/2025_10.ts
@@ -0,0 +1,207 @@
+const Posts_2025_10 = [
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/19oaks?lang=eng",
+    "title": "Introduction",
+    "speaker": "Dallin H. Oaks",
+    "audioUrl": "https://assets.churchofjesuschrist.org/oiy360tmcpkfdgi1m8qqei26hylrlsuyyv2oszeb-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/12stevenson?lang=eng",
+    "title": "Blessed Are the Peacemakers",
+    "speaker": "Gary E. Stevenson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/q3xnzwtyox9ne5jkpb6l9rtnrqrsxjgi9w2jgpja-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/13browning?lang=eng",
+    "title": "Tune Your Heart to Jesus Christ: The Sacred Gift of Primary Music",
+    "speaker": "Tracy Y. Browning",
+    "audioUrl": "https://assets.churchofjesuschrist.org/v3x756ama9pgfivrhmq2yolp1yo7gd4cqage55q7-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/14barcellos?lang=eng",
+    "title": "The Lord Looketh on the Heart",
+    "speaker": "Ronald M. Barcellos",
+    "audioUrl": "https://assets.churchofjesuschrist.org/4yiquxpg2m5biyr5ofx4a74cpw6x2fwn1c9mx47z-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/15eyre?lang=eng",
+    "title": "Know Who You Really Are",
+    "speaker": "Brik V. Eyre",
+    "audioUrl": "https://assets.churchofjesuschrist.org/r7wkgayc6xjhbgzd221gfo0fuu4kc0hip7w0k7uj-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/17johnson?lang=eng",
+    "title": "Be Reconciled to God",
+    "speaker": "Kelly R. Johnson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/77m6agrxmg6mwydbae5iwdi8qce2ae8kabri2cyv-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/16uchtdorf?lang=eng",
+    "title": "Do Your Part with All Your Heart",
+    "speaker": "Dieter F. Uchtdorf",
+    "audioUrl": "https://assets.churchofjesuschrist.org/0x977mc68w8wptl5jcflivvg5bq36sbcu34v3a87-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/21rasband?lang=eng",
+    "title": "The Family Proclamation Words from God",
+    "speaker": "Ronald A. Rasband",
+    "audioUrl": "https://assets.churchofjesuschrist.org/myo4we3g0s5kcf1d4whaha7j9jiztpjorlrljfl9-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/22webb?lang=eng",
+    "title": "That All May Be Edified",
+    "speaker": "Chad H Webb",
+    "audioUrl": "https://assets.churchofjesuschrist.org/w92ddkh60dhxlb553cqyuz0lpejw17jbox0e60l3-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/23jaggi?lang=eng",
+    "title": "Humble Souls at Altars Kneel",
+    "speaker": "Jeremy R. Jaggi",
+    "audioUrl": "https://assets.churchofjesuschrist.org/ilhpztoike1schgbv6rlgb8ja9olhy0ng4lsoaqr-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/24brown?lang=eng",
+    "title": "The Eternal Gift of Testimony",
+    "speaker": "Kevin G. Brown",
+    "audioUrl": "https://assets.churchofjesuschrist.org/66rd9qwftmlfxwpaxq6490zv9k9s1hxdzmrnvz9i-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/25gong?lang=eng",
+    "title": "No One Sits Alone",
+    "speaker": "Gerrit W. Gong",
+    "audioUrl": "https://assets.churchofjesuschrist.org/51gxoms4pd5tpbc8gyfuem3z495wnht7wikv8b91-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/26cziesla?lang=eng",
+    "title": "Simplicity in Christ",
+    "speaker": "Michael Cziesla",
+    "audioUrl": "https://assets.churchofjesuschrist.org/3e55qbm7cpy0rh327lzfw0bhda9873qixugp6ffc-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/27cook?lang=eng",
+    "title": "The Lord Is Hastening His Work",
+    "speaker": "Quentin L. Cook",
+    "audioUrl": "https://assets.churchofjesuschrist.org/3s9ihslk5nxls0qhdcf1x623ct5gexbm2p9qgree-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/31kearon?lang=eng",
+    "title": "Jesus Christ and Your New Beginning",
+    "speaker": "Patrick Kearon",
+    "audioUrl": "https://assets.churchofjesuschrist.org/tc6cc5xsi14xet439s0wj0qb1b233st1u09bfm93-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/32dennis?lang=eng",
+    "title": "Cheering Each Other On",
+    "speaker": "J. Anette Dennis",
+    "audioUrl": "https://assets.churchofjesuschrist.org/pty6pfuxza821mqnycx0khxnke63uco0zuoanh30-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/33barlow?lang=eng",
+    "title": " Lovest Thou Me? ",
+    "speaker": "Steven C. Barlow",
+    "audioUrl": "https://assets.churchofjesuschrist.org/cbjvwblwn00srk52nu48shf50tenm0ozukkszcgb-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/34jackson?lang=eng",
+    "title": "Remembering the Sheep",
+    "speaker": "William K. Jackson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/q7efpa4qr14d7g7e8ffb9lu0musgos8h5dqxosum-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/35andersen?lang=eng",
+    "title": "The Atoning Love of Jesus Christ",
+    "speaker": "Neil L. Andersen",
+    "audioUrl": "https://assets.churchofjesuschrist.org/8278d3tpgl6e53024da93mcgmantkydllwdkqd4w-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/41holland?lang=eng",
+    "title": "And Now I See",
+    "speaker": "Jeffrey R. Holland",
+    "audioUrl": "https://assets.churchofjesuschrist.org/q57j423iko0mmmzivy5ibhcydx1s2pz3hozycn4r-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/42evanson?lang=eng",
+    "title": "Go and Do Likewise",
+    "speaker": "James E. Evanson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/6newkuxpux20s4op0r0jagwtma1l9jjrpvgidse5-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/43soares?lang=eng",
+    "title": "Adorned with the Virtue of Temperance",
+    "speaker": "Ulisses Soares",
+    "audioUrl": "https://assets.churchofjesuschrist.org/kvnda5z79j2mt9khl41c8c1emmoj1gmxfxauuusz-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/44johnson?lang=eng",
+    "title": "The Power of Ministering to the One",
+    "speaker": "Peter M. Johnson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/3ydg7f6yu9rmfqkqvlwdepovmijuxnw1m3tjv3p0-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/45christofferson?lang=eng",
+    "title": "Look to God and Live",
+    "speaker": "D. Todd Christofferson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/eiqooznf3qhuasvxgp92v6jhn4vsg7uipo685f8p-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/46spannaus?lang=eng",
+    "title": "Prophets of God",
+    "speaker": "Andrea Mu oz Spannaus",
+    "audioUrl": "https://assets.churchofjesuschrist.org/zq8979lo2ty683yrhrxxthq4h3vdr3t71dg2a3ct-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/47eyring?lang=eng",
+    "title": "Proved and Strengthened in Christ",
+    "speaker": "Henry B. Eyring",
+    "audioUrl": "https://assets.churchofjesuschrist.org/fb7jfw7kf07jevafigmog2w11eryybyrs7aa3in0-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/51bednar?lang=eng",
+    "title": "They Are Their Own Judges",
+    "speaker": "David A. Bednar",
+    "audioUrl": "https://assets.churchofjesuschrist.org/d7tzhc6lzmkokog32go99mya6r1s64rkwxrseur6-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/52cuvelier?lang=eng",
+    "title": "The Name by Which Ye Are Called",
+    "speaker": "B. Corey Cuvelier",
+    "audioUrl": "https://assets.churchofjesuschrist.org/kyzsduqd23wt5c7ye5ufn9iv015ofmord3lg0wlt-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/53holland?lang=eng",
+    "title": "Forsake Not Your Own Mercy",
+    "speaker": "Matthew S. Holland",
+    "audioUrl": "https://assets.churchofjesuschrist.org/dnrn2ptkrcnylngw4mutdr0gpr6xi6ootj0251fc-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/54godoy?lang=eng",
+    "title": "Smiling Faces and Grateful Hearts",
+    "speaker": "Carlos A. Godoy",
+    "audioUrl": "https://assets.churchofjesuschrist.org/vtgcxbwamakfj1s0kn7fan8pyju67skyq5uu0xza-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/55renlund?lang=eng",
+    "title": "Taking on the Name of Jesus Christ",
+    "speaker": "Dale G. Renlund",
+    "audioUrl": "https://assets.churchofjesuschrist.org/ng9ysi3xdzhce7gelr0u42lfesl6odhi9dlnh7gr-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/56amos?lang=eng",
+    "title": "The Good News Recipe",
+    "speaker": "John D. Amos",
+    "audioUrl": "https://assets.churchofjesuschrist.org/00d4wwg256tnpppq4rfxh0o11oqjzk593lltpg0d-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/57farias?lang=eng",
+    "title": "The Book of Mormon an Immeasurable Treasure on Our Journey",
+    "speaker": "Ozani Farias",
+    "audioUrl": "https://assets.churchofjesuschrist.org/aiybrpwoqlwangyk5lgg9jeh8jmuczio3j272jjy-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2025/10/58oaks?lang=eng",
+    "title": "The Family-Centered Gospel of Jesus Christ",
+    "speaker": "Dallin H. Oaks",
+    "audioUrl": "https://assets.churchofjesuschrist.org/rfxti4y9dyb8s3i44lptmqc62zpmxeni4krftf0z-128k-en.mp3?download=true"
+  }
+] 
+ export default Posts_2025_10
