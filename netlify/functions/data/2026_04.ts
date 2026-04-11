218186805ad55e4ca729af78a6b8f795d669655d
Daniel Clayson <daniel@dabblefox.com>
2026-04-11 15:40:42 -0400

    feat: Add 2026_04

---
+++ netlify/functions/data/2026_04.ts
@@ -0,0 +1,213 @@
+const Posts_2026_04 = [
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/11oaks?lang=eng",
+    "title": "Introduction",
+    "speaker": "Dallin H. Oaks",
+    "audioUrl": "https://assets.churchofjesuschrist.org/065kn0zd5mrfs7it2ixgmz5j65sc25jdm2tymc1u-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/12christofferson?lang=eng",
+    "title": "Solemn Assembly",
+    "speaker": "D. Todd Christofferson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/sujtq4ksfdwqnrn7onwwi3v38j131q79vvm0lf4i-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/13kearon?lang=eng",
+    "title": "About His Business",
+    "speaker": "Patrick Kearon",
+    "audioUrl": "https://assets.churchofjesuschrist.org/fa9olrfp6tpor391zmvyieersvwr31ka0fep0hup-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/14yee?lang=eng",
+    "title": "Ministering  That Ye Love One Another; as I Have Loved You ",
+    "speaker": "Kristin M. Yee",
+    "audioUrl": "https://assets.churchofjesuschrist.org/4f2spelejzldevzj894p3jsf5ctcv5ixme7eq3qs-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/15gilbert?lang=eng",
+    "title": "Come Home",
+    "speaker": "Clark G. Gilbert",
+    "audioUrl": "https://assets.churchofjesuschrist.org/j8nliqaux226sufyimnvu59gu2ltv27tou2uwrm7-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/16bednar?lang=eng",
+    "title": "All Who Have Endured Valiantly",
+    "speaker": "David A. Bednar",
+    "audioUrl": "https://assets.churchofjesuschrist.org/zrcbiszst15rxdbhwuukflpsyqra0v2kbcjln34a-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/17teh?lang=eng",
+    "title": "Follow the Prophet; He Knows the Way",
+    "speaker": "Michael John U. Teh",
+    "audioUrl": "https://assets.churchofjesuschrist.org/apr8hu31z2owmfezmq5xfj9r802bf5ohwa2crpxs-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/18becerra?lang=eng",
+    "title": "Tithing Putting God First",
+    "speaker": "Jorge T. Becerra",
+    "audioUrl": "https://assets.churchofjesuschrist.org/e339lcvyoz47yjy9aywgzrkwxkvtnv24hhvyjkv9-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/19eyring?lang=eng",
+    "title": "Prayers for Peace",
+    "speaker": "Henry B. Eyring",
+    "audioUrl": "https://assets.churchofjesuschrist.org/yt6n7p2tzterftnvmqix247nahe8cjpkqvk1cct1-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/23stevenson?lang=eng",
+    "title": "Lost Luggage, Redeemed Souls",
+    "speaker": "Gary E. Stevenson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/0gh8vferugnavn1voi14tchoriigg2dw0nfsp819-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/24ortega?lang=eng",
+    "title": "Christ Author and Finisher of Our Faith",
+    "speaker": "Eduardo F. Ortega",
+    "audioUrl": "https://assets.churchofjesuschrist.org/fi7tiszje7a0j3m014kitbwgttdqyvpllddpkabd-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/25wu?lang=eng",
+    "title": " I Will Give Away All My Sins to Know Thee ",
+    "speaker": "Wan-Liang Wu",
+    "audioUrl": "https://assets.churchofjesuschrist.org/rb5poiwuy9phqo6zuc8ew7xsv73uto9rq9zsy5zj-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/26wunderli?lang=eng",
+    "title": "Jesus Christ Is Not Our Burden; He Is Our Relief",
+    "speaker": "David J. Wunderli",
+    "audioUrl": "https://assets.churchofjesuschrist.org/5c0dnq676ig81cycgqy82t6d97du1t2hmevncngd-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/27causse?lang=eng",
+    "title": "Love All; Love Each",
+    "speaker": "G rald Causs ",
+    "audioUrl": "https://assets.churchofjesuschrist.org/p7xaqzuzxvl8dth3j60b8uk79mepp92agxre7l2t-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/28holmes?lang=eng",
+    "title": "Jesus Christ Is the Way",
+    "speaker": "Brian J. Holmes",
+    "audioUrl": "https://assets.churchofjesuschrist.org/ybwz3ejkmkfijcgrctc3diaebfqede7i209d76ew-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/29matswagothata?lang=eng",
+    "title": "He Knows You by Name",
+    "speaker": "Clement M. Matswagothata",
+    "audioUrl": "https://assets.churchofjesuschrist.org/4zpq2ym21gbmzl19b0c831h5h7js2yh25js13d70-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/210soares?lang=eng",
+    "title": "Jesus Christ the True Vine",
+    "speaker": "Ulisses Soares",
+    "audioUrl": "https://assets.churchofjesuschrist.org/0gjd6w02gis5gtv069vclwknyjql4p8akdd3jts9-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/41uchtdorf?lang=eng",
+    "title": "Encounter at the Empty Tomb",
+    "speaker": "Dieter F. Uchtdorf",
+    "audioUrl": "https://assets.churchofjesuschrist.org/r25iylnzloy3drk96cz9q5nmmgm98e4fvibperlq-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/42freeman?lang=eng",
+    "title": "Best Days and Worst Days",
+    "speaker": "Emily Belle Freeman",
+    "audioUrl": "https://assets.churchofjesuschrist.org/399xzzcdtaxbyusiq3w6e5yn12mo8s0to58bzv1v-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/43larreal?lang=eng",
+    "title": "I Feel My Savior s Love",
+    "speaker": "Pedro X. Larreal",
+    "audioUrl": "https://assets.churchofjesuschrist.org/s9uezgcrou2i3gh93s57lxhbkbuwag7msg6yp161-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/44rowe?lang=eng",
+    "title": "Choose Jesus Christ as Your Guide",
+    "speaker": "Edward B. Rowe",
+    "audioUrl": "https://assets.churchofjesuschrist.org/3i2e4fzzftrdjr7m8fxdx8vguib7h70asumdgnwd-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/45rasband?lang=eng",
+    "title": "He Is Risen",
+    "speaker": "Ronald A. Rasband",
+    "audioUrl": "https://assets.churchofjesuschrist.org/y7gqrc06qwccv2m11f9tqzh6wp7ff9ucemgqq18g-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/46renlund?lang=eng",
+    "title": "Because of Jesus Christ",
+    "speaker": "Dale G. Renlund",
+    "audioUrl": "https://assets.churchofjesuschrist.org/zl46y0kbbduibglsnxodxiz1crz0wqzimduxn25r-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/47mutombo?lang=eng",
+    "title": "The Joy of a Covenant Relationship with God",
+    "speaker": "Thierry K. Mutombo",
+    "audioUrl": "https://assets.churchofjesuschrist.org/5sjq7nipv3hcxb6nf8sjg0j74udigyact0qj16cg-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/48walker?lang=eng",
+    "title": "A Peculiar Treasure",
+    "speaker": "Alan R. Walker",
+    "audioUrl": "https://assets.churchofjesuschrist.org/t69cad4xyzicz6zqrrqour8v6528jyztmctb6a5d-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/49oaks?lang=eng",
+    "title": "Alive in Christ",
+    "speaker": "Dallin H. Oaks",
+    "audioUrl": "https://assets.churchofjesuschrist.org/tco9uxq9rmy3218fxamd9whrr6m2nvwfzcp27l42-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/51christofferson?lang=eng",
+    "title": "The Character of Christ",
+    "speaker": "D. Todd Christofferson",
+    "audioUrl": "https://assets.churchofjesuschrist.org/e7w4rp9xbff65fi0i7dqbf3qzffk3mpdsba8ijtp-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/52wong?lang=eng",
+    "title": "Remember, Remember",
+    "speaker": "Chi Hong (Sam) Wong",
+    "audioUrl": "https://assets.churchofjesuschrist.org/68jm6mvckrcej07n1cba1gmrs3nlbvw2mzky5miv-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/53hall?lang=eng",
+    "title": "I Glory in My Jesus",
+    "speaker": "Aaron T. Hall",
+    "audioUrl": "https://assets.churchofjesuschrist.org/7o7cq78jmiwkkgvjzo2kbl4o1bq4syk8fl9alce8-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/54porter?lang=eng",
+    "title": " Here Am I, Send Me ",
+    "speaker": "Susan H. Porter",
+    "audioUrl": "https://assets.churchofjesuschrist.org/x9clbk1xcjl7zti5thezkcufxt5s72yj5ncfll82-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/55andersen?lang=eng",
+    "title": "Eternal Marriage Is an Eternal Journey",
+    "speaker": "Neil L. Andersen",
+    "audioUrl": "https://assets.churchofjesuschrist.org/y66s81zg8ikr2l0d956z37tsarcopzexcogr8u99-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/56cook?lang=eng",
+    "title": "Keys, Covenants, and Easter",
+    "speaker": "Quentin L. Cook",
+    "audioUrl": "https://assets.churchofjesuschrist.org/a0ikyusnmid6yi75wh1eoflwzejxbsulqqdo4kzt-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/57wakolo?lang=eng",
+    "title": "Come unto Christ Together",
+    "speaker": "Taniela B. Wakolo",
+    "audioUrl": "https://assets.churchofjesuschrist.org/rt3ruv05ut32ntskcxl629lb9vd6oafafg8hhrtl-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/58gong?lang=eng",
+    "title": " Tis Eastertide: No One Walks Alone",
+    "speaker": "Gerrit W. Gong",
+    "audioUrl": "https://assets.churchofjesuschrist.org/ca6qcutqpwsky40azve38ch9gck5kk11qlkiwg00-128k-en.mp3?download=true"
+  },
+  {
+    "href": "https://www.churchofjesuschrist.org/study/general-conference/2026/04/59oaks?lang=eng",
+    "title": "Closing Remarks",
+    "speaker": "Dallin H. Oaks",
+    "audioUrl": "https://assets.churchofjesuschrist.org/l66awxxycj8ifet93izjar84wvvu7k2ioj5mupz6-128k-en.mp3?download=true"
+  }
+] 
+ export default Posts_2026_04
